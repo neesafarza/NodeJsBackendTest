6edfa7b82d3bb36d379f4b1b7db3c499ed2f1a8b
neesafarza <aneesazafri@gmail.com>
2020-11-10 12:09:18 +0000

    updatePort

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const mongoose = require('./models/index')
 const router = require('./router');
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
